4f6910f26727ea94f1c37f7c9d9d9d8f0369bbbe
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-12 22:23:10 +0530

    html links

--- crawler.py
+++ crawler.py
@@ -1,10 +1,22 @@
 import requests
 import re
+from urllib.parse import urlparse
 
 class PyCrawler(object):
     def __init__(self, starting_url):
         self.starting_url = starting_url
         self.visited = set()
+
+    def get_html(self, url):
+        try:
+            html = requests.get(url)
+        except Exception as e:
+            print(e)
+            return ""
+        return html.content.decode('latin-1')
+
+    def get_links(self, url):
+        pass
 
     def start(self):
         pass
